cf4e7fcaabebe96a1d8ffa4321bf6af1714e2246
Luciano Evaristo Guerche <guercheLE@gmail.com>
2016-09-10 13:09:21 -0300

    Enclosed angular application into an Immediately-Invoked Function Expression (IIFE) and fixed split character to be comma

--- assignments/assignment1/assignment1-starter-code/index.js
+++ assignments/assignment1/assignment1-starter-code/index.js
@@ -1,27 +1,29 @@
-'use strict';
+(function (){
+    'use strict';
 
-angular.module('Assignment1App', [])
-    .controller('Assignment1Controller1', ['$scope', function($scope) {
-        $scope.dishes = '';
-        $scope.message = '';
-        $scope.CheckIfTooMuch = function() {
-            var cleanedUpInput = $scope.dishes.replace(/\s+;/g, ';')
-                .replace(/;\s+/g, ';')
-                .replace(/;;/g, ';')
-                .replace(/^;/g, '')
-                .replace(/;$/g, '');
+    angular.module('Assignment1App', [])
+        .controller('Assignment1Controller1', ['$scope', function($scope) {
+            $scope.dishes = '';
+            $scope.message = '';
+            $scope.CheckIfTooMuch = function() {
+                var cleanedUpInput = $scope.dishes.replace(/\s+,/g, ',')
+                    .replace(/,\s+/g, ',')
+                    .replace(/,,/g, ',')
+                    .replace(/^,/g, '')
+                    .replace(/,$/g, '');
 
-            if (cleanedUpInput === '') {
-                $scope.message = 'Please enter data first';
-                return;
+                if (cleanedUpInput === '') {
+                    $scope.message = 'Please enter data first';
+                    return;
+                }
+
+                var dishes = cleanedUpInput.split(',');
+
+                if (dishes.length <= 3) {
+                    $scope.message = 'Enjoy!';
+                } else {
+                    $scope.message = 'Too much!';
+                }
             }
-
-            var dishes = cleanedUpInput.split(';');
-
-            if (dishes.length <= 3) {
-                $scope.message = 'Enjoy!';
-            } else {
-                $scope.message = 'Too much!';
-            }
-        }
-    }]);
+        }]);
+})();
